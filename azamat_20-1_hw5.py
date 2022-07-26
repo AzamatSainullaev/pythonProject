d0d2eccaeb3ac8e44c7617c8b0b231a80cfc97dd
azamat <aza.sainulaev@gmail.com>
2022-07-26 15:13:08 +0300

    azamat

--- azamat_20-1_hw5.py
+++ azamat_20-1_hw5.py
@@ -1,11 +1,17 @@
-#HW:
-#написать валидацию на Номера Транспартов
-#будет класс ValidCarNumber —> будет метод is_valid(self, number)
-#который принимает 1 аргумент number и проверяет на валидность то есть:
+import re
 
-#Import re
+
 class ValidCarNumber:
-    def __init__(self,numbor):
-        self.numbor=numbor
+    def __init__(self, number):
+        self.number = number
     def is_valid(self):
+        is_valid = re.search(r"0([0-9]{1})KG([0-9]{3})([A-Z]{3})", self.number)
+        try:
+            if self.number[is_valid.start():is_valid.end()] == self.number:
+                print("Valid")
+        except:
+            print("Invalid")
 
+number = input('Пример: 01KG777BAD \nВведите номер транспарта: ')
+car = ValidCarNumber(number)
+car.is_valid()
